a7c3df3ae2a3d2dd4b08ed943f19c70108e07692
Bang Tedy <tedysuwega@gmail.com>
2019-05-13 19:09:01 +0700

    update download pic

--- route.py
+++ route.py
@@ -23,7 +23,7 @@ def say_name_json(name):
 
 @app.route('/downloadImage/<name>')
 def downloadImage(name):
-    url ="https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fthedailybugle%2Fimages%2F2%2F2b%2FUltimate_Spider_Man_Render.png%2Frevision%2Flatest%3Fcb%3D20160319202253&imgrefurl=https%3A%2F%2Fthedailybugle.fandom.com%2Fwiki%2FSpider-Man&docid=I1FFzCCnUAsXMM&tbnid=1SMQP1XEsdMgCM%3A&vet=10ahUKEwjj6pmDuJjiAhVU8HMBHdTFBXsQMwiQASgZMBk..i&w=1778&h=2700&safe=strict&bih=969&biw=1920&q=spiderman&ved=0ahUKEwjj6pmDuJjiAhVU8HMBHdTFBXsQMwiQASgZMBk&iact=mrc&uact=8"
+    url ="https://o.aolcdn.com/images/dims?quality=85&image_uri=https%3A%2F%2Fs.aolcdn.com%2Fhss%2Fstorage%2Fmidas%2Fdba1b71c87c7ef0d2ce3cd26dd1eb15e%2F206449491%2FMSM_Screen_PS4Pro_E32018_00003.jpg&client=amp-blogside-v2&signature=9a8deccbe35c8d1c98ebdc980ecc27abf926f761"
     md.downloadImage(url,name)
     return "got it"
 
